67ab8372a7f6b974293d27af98cb368a6402f2da
Sazz <shrishkadriya94@gmail.com>
2025-04-12 18:56:45 +0545

    script.js

---
+++ voice box/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#navbar-container").load("navbar.html");
+  
+    new Typed("#typed-text", {
+      strings: ["Welcome to Zenmandu"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop: true
+    });
+  });
+  
